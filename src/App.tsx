9ccb5827530e8f712deaea2771b6f20d0d03830f
nastiaknik <anastazja.knihnitska@gmail.com>
2023-08-30 12:40:18 +0200

    set up cra with ts template

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "97vh",
+        fontSize: 24,
+      }}
+    >
+      TypeScript
+    </div>
+  );
+}
+
+export default App;
